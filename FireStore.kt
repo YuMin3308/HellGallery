38681c39033c26add7ffa7e33e3406a07a1ef713
jimyoung98 <90492795+jimyoung98@users.noreply.github.com>
2021-11-09 09:34:59 +0900

    Add files via upload

---
+++ FireStore.kt
@@ -0,0 +1,9 @@
+package com.example.teamp
+
+data class FireStore(
+    var email : String,
+    var exp : Long = 0,
+    var level : Long = 1,
+    var height : Long = 0,
+    var weight : Long = 0,
+)
