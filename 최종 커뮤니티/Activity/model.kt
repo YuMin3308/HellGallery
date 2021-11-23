8b1bfff98da24e8d8bedcb367896549453a9917e
SangLyulLee <90492752+SangLyulLee@users.noreply.github.com>
2021-11-23 13:28:26 +0900

    Create model.kt

---
+++ 최종 커뮤니티/Activity/model.kt
@@ -0,0 +1,10 @@
+package com.example.logo.Community
+
+//커뮤니티 데이터 모델
+data class Model (
+    val title : String = "",  //글제목
+    val content : String = "", //내용
+    val uid : String = "", //uid number
+    val time : String = "", //시간
+    val cid : String = ""
+)
